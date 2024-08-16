8bbde06fc6194068e5c216d3c6f324fbbe5e1054
var-code-5 <varshithisgod@gmail.com>
2024-08-16 22:52:51 +0530

    feat: init

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send({"message": "bow bow"});
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send({"message": "Success!"});
+});
+
+app.listen(port,(res,req)=>{
+    console.log(`listening on ${port}`);
+});
