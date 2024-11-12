20156d5010f12779cc04ba7c19a76284016813f9
var-code-5 <varshithisgod@gmail.com>
2024-11-12 22:57:28 +0530

    fix : recive json data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send({"message": "bow bow"});
